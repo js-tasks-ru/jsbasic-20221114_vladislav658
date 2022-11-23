7ca43ee6864918619c432b57b2bdd791c41f3d5f
Vladislav Pasternak <gillgalad1971@gmail.com>
2022-11-23 09:11:05 +0300

    Сделал (дубль 2, с учетом замечаний) /1-module/1-task/

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,12 +1,14 @@
 function factorial(n) {
   // от vladislav658
-  let factor;
-  factor = 1;
-  if (n == 0 || n == 1) {
+  let factor = 1;
+  if (n < 0) {
+    alert("Факториала отрицательных чисел не бывает!");
+  } else if (n == 0 || n == 1) {
+    return (factor);	  
   } else {
     for ( i = 1; i <= n; i++) {
       factor = factor * i;
     }
+    return (factor);
   }
-  return (factor);
 }
