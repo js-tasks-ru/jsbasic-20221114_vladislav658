38be860f046a7a248796f72f66c49c10986d5711
Vladislav Pasternak <gillgalad1971@gmail.com>
2022-11-25 08:51:45 +0300

    Сделал /1-module/3-task/ (дубль 2, после первого просмотра)

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,12 +1,7 @@
 // от vladislav658
 function ucFirst(str) {
-  let strValid = str;
-  if (strValid === null || strValid === undefined) {
-    console.log('Данные не корректны, НЕ подразумевается что содержимое null или undefined...');
-	return (false);
-  } else {
-    const strValidUpper = strValid.charAt(0).toUpperCase() + strValid.substring(1);
-    console.log('Данные корректны... = ', strValidUpper);
-    return (strValidUpper);	
+  if (!str) {
+    return str;
   }
+  return str.charAt(0).toUpperCase() + str.substring(1);	
 }
