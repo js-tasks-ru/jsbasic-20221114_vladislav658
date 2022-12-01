0132c4482c3002b6a1b4f73fe50b627684d9b799
Vladislav Pasternak <gillgalad1971@gmail.com>
2022-12-01 18:22:45 +0300

    Сделал /3-module/4-task/

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,9 @@
+// от vladislav658
 function showSalary(users, age) {
-  // ваш код...
+	
+  getUsersFilter = users
+    .filter(users => (users.age <= age))
+    .map(users => (users.name +  ", " + users.balance));
+	
+  return getUsersFilter.join('\n');
 }
